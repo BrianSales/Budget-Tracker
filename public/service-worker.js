9efc9c6b10dd7e1610f0dfbb0519804c730651f5
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-06-09 14:09:23 -0500

    added a activate listener to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,9 @@ const Static = 'static-cache-v2'
 
 self.addEventListener('install', function (event){
     event.waitUntil(
-        caches.open(Static).then(cache =>{
+        caches
+        .open(Static)
+        .then(cache =>{
             console.log("Pre-cached successfully");
             return cache.addAll(Cache_Files)
         })
@@ -19,3 +21,17 @@ self.addEventListener('install', function (event){
 
     self.skipWaiting();
 });
+
+seld-addEventListener('activate', function (event){
+    event.waitUntil(
+        caches.keys().then(keylist => {
+            return Promise.all(
+                keylist.map(key =>{
+                    console.log('Old key removed', key);
+                    return caches.delete(key)
+                })
+            )
+        })
+    )
+    self.clients.claim
+})
