abbd078d2c81025518a5b8b396653541f4aef494
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-06-09 15:17:33 -0500

    i think service-worker is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,74 @@
-const Cache_Files =[
-    "/",
-    "/index.html", 
-    "/index.js",      
-    "/styles.css", 
-
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
 ];
-
-const Cache_run = 'runtime-cache'
-const Static = 'static-cache-v2'
-
-self.addEventListener('install', function (event){
-    event.waitUntil(
-        caches
-        .open(Static)
-        .then(cache =>{
-            console.log("Pre-cached successfully");
-            return cache.addAll(Cache_Files)
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Old cache data removed", key);
+            return caches.delete(key);
+          }
         })
+      );
+    })
+  );
+  self.clients.claim();
+});
+// Fetch Files
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    console.log("[Service Worker] Fetch (data)", event.request.url);
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
-
-    self.skipWaiting();
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
 });
 
-seld-addEventListener('activate', function (event){
-    const Caches = [Static, Cache_run]
-    event.waitUntil(
-        caches.keys()
-        .then(cacheValue=>
-            cacheValue.filter(cacheValue => !currentCaches.includes(CacheValue))
-            )
-            .then(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete)))
 
-    )
 
-    .then(() => self.clients.claim())
-    
-})
+
+
+
+
